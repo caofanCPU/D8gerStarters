06f6eb3d68983c904f26567c331b4f293f14c8da
D8GER <xyb5to0ZCY@gmail.com>
2020-07-24 17:03:54 +0800

    swagger api, add validate rule for doc view

--- spring-cloud-starter-d8ger-web-util/src/main/java/com/xyz/caofancpu/mvc/configuration/SwaggerConfiguration.java
+++ spring-cloud-starter-d8ger-web-util/src/main/java/com/xyz/caofancpu/mvc/configuration/SwaggerConfiguration.java
@@ -29,6 +29,7 @@ import com.xyz.caofancpu.core.CollectionUtil;
 import com.xyz.caofancpu.property.SwaggerProperties;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
@@ -56,6 +57,15 @@ import springfox.documentation.swagger.schema.ApiModelProperties;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import javax.annotation.Resource;
+import javax.validation.constraints.DecimalMax;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.lang.reflect.Field;
 import java.util.Arrays;
 import java.util.List;
@@ -154,10 +164,22 @@ public class SwaggerConfiguration {
          */
         private static final String MODEL_DESCRIPTION_KEY = "description";
 
+        /**
+         * 校验规则标题
+         */
+        public static final String VALIDATE_TITLE_KEY = "校验规则: ";
+
         @Override
         public void apply(ModelPropertyContext context) {
+            String fieldDescription = SymbolConstantUtil.EMPTY;
+            try {
+                Field mField = ModelPropertyBuilder.class.getDeclaredField(MODEL_DESCRIPTION_KEY);
+                mField.setAccessible(true);
+                fieldDescription += mField.get(context.getBuilder());
+            } catch (Exception e) {
+                log.error(e.getMessage());
+            }
             Optional<ApiModelProperty> annotation = Optional.empty();
-
             if (context.getAnnotatedElement().isPresent()) {
                 annotation = Optional.of(annotation.orElseGet(ApiModelProperties.findApiModePropertyAnnotation(context.getAnnotatedElement().get())::get));
             }
@@ -165,23 +187,67 @@ public class SwaggerConfiguration {
                 annotation = Optional.of(annotation.orElseGet(Annotations.findPropertyAnnotation(context.getBeanPropertyDefinition().get(), ApiModelProperty.class)::get));
             }
             final Class<?> rawPrimaryType = context.getBeanPropertyDefinition().get().getRawPrimaryType();
-            // 过滤得到目标类型
+            // 处理枚举类型
             if (annotation.isPresent() && IEnum.class.isAssignableFrom(rawPrimaryType)) {
                 List<String> displayValueList = CollectionUtil.transToList(Arrays.asList(rawPrimaryType.getEnumConstants()), item -> {
                     IEnum iEnum = (IEnum) item;
                     return iEnum.getValue() + SymbolConstantUtil.ENGLISH_COLON + iEnum.getName();
                 });
-                String enumDescription = SymbolConstantUtil.SPACE + SymbolConstantUtil.ENGLISH_LEFT_BRACKET + String.join("; ", displayValueList) + SymbolConstantUtil.ENGLISH_RIGHT_BRACKET;
-                try {
-                    Field mField = ModelPropertyBuilder.class.getDeclaredField(MODEL_DESCRIPTION_KEY);
-                    mField.setAccessible(true);
-                    enumDescription = mField.get(context.getBuilder()) + enumDescription;
-                } catch (Exception e) {
-                    log.error(e.getMessage());
-                }
+                String enumDescription = SymbolConstantUtil.SPACE + SymbolConstantUtil.ENGLISH_LEFT_BRACKET + String.join(SymbolConstantUtil.ENGLISH_SEMICOLON + SymbolConstantUtil.SPACE, displayValueList) + SymbolConstantUtil.ENGLISH_RIGHT_BRACKET;
+                fieldDescription += enumDescription;
                 final ResolvedType resolvedType = context.getResolver().resolve(int.class);
-                context.getBuilder().description(enumDescription).type(resolvedType);
+                context.getBuilder().type(resolvedType);
             }
+            String validateMessage = normalValidateMessage(context);
+            if (StringUtils.isNotBlank(validateMessage)) {
+                fieldDescription += SymbolConstantUtil.ERROR_SHOW + VALIDATE_TITLE_KEY + validateMessage + SymbolConstantUtil.ERROR_SHOW;
+            }
+            context.getBuilder().description(fieldDescription);
+        }
+
+        @SuppressWarnings("Guava")
+        private String normalValidateMessage(ModelPropertyContext context) {
+            // 处理校验注解
+            List<String> validAnnotationMessageList = Lists.newArrayList();
+            if (context.getBeanPropertyDefinition().isPresent()) {
+                com.google.common.base.Optional<NotNull> notNull = Annotations.findPropertyAnnotation(context.getBeanPropertyDefinition().get(), NotNull.class);
+                if (notNull.isPresent()) {
+                    validAnnotationMessageList.add(notNull.get().message());
+                }
+                com.google.common.base.Optional<NotBlank> notBlank = Annotations.findPropertyAnnotation(context.getBeanPropertyDefinition().get(), NotBlank.class);
+                if (notBlank.isPresent()) {
+                    validAnnotationMessageList.add(notBlank.get().message());
+                }
+                com.google.common.base.Optional<NotEmpty> notEmpty = Annotations.findPropertyAnnotation(context.getBeanPropertyDefinition().get(), NotEmpty.class);
+                if (notEmpty.isPresent()) {
+                    validAnnotationMessageList.add(notEmpty.get().message());
+                }
+                com.google.common.base.Optional<Digits> digits = Annotations.findPropertyAnnotation(context.getBeanPropertyDefinition().get(), Digits.class);
+                if (digits.isPresent()) {
+                    validAnnotationMessageList.add(digits.get().message());
+                }
+                com.google.common.base.Optional<DecimalMax> decimalMax = Annotations.findPropertyAnnotation(context.getBeanPropertyDefinition().get(), DecimalMax.class);
+                if (decimalMax.isPresent()) {
+                    validAnnotationMessageList.add(decimalMax.get().message());
+                }
+                com.google.common.base.Optional<DecimalMin> decimalMin = Annotations.findPropertyAnnotation(context.getBeanPropertyDefinition().get(), DecimalMin.class);
+                if (decimalMin.isPresent()) {
+                    validAnnotationMessageList.add(decimalMin.get().message());
+                }
+                com.google.common.base.Optional<Min> min = Annotations.findPropertyAnnotation(context.getBeanPropertyDefinition().get(), Min.class);
+                if (min.isPresent()) {
+                    validAnnotationMessageList.add(min.get().message());
+                }
+                com.google.common.base.Optional<Max> max = Annotations.findPropertyAnnotation(context.getBeanPropertyDefinition().get(), Max.class);
+                if (max.isPresent()) {
+                    validAnnotationMessageList.add(max.get().message());
+                }
+                com.google.common.base.Optional<Size> size = Annotations.findPropertyAnnotation(context.getBeanPropertyDefinition().get(), Size.class);
+                if (size.isPresent()) {
+                    validAnnotationMessageList.add(size.get().message());
+                }
+            }
+            return CollectionUtil.join(validAnnotationMessageList, SymbolConstantUtil.ENGLISH_SEMICOLON + SymbolConstantUtil.SPACE);
         }
 
         @Override
